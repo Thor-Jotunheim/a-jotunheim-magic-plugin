30a730aee699360fc3c73921c2f3b0e37110427a
Thor-Jotunheim <138218017+Thor-Jotunheim@users.noreply.github.com>
2024-11-25 11:45:43 -0600

    trade

--- includes/Trade/barter-page-setup.php
+++ includes/Trade/barter-page-setup.php
@@ -44,17 +44,29 @@ function jotunheim_enqueue_barter_scripts() {
 }
 add_action('wp_enqueue_scripts', 'jotunheim_enqueue_barter_scripts');
 
-// Search Bar Shortcode (Reusable for left or right)
-function render_search_bar($atts) {
+// Search Bar Shortcode (Handles both search bar and sidebar accordion)
+function render_search_bar_with_sidebar($atts) {
     $atts = shortcode_atts([
-        'id' => 'item-search',
-        'placeholder' => 'Search items...',
-        'target' => 'item-list-accordion'
+        'id' => 'item-search', // ID of the search bar
+        'placeholder' => 'Search items...', // Placeholder for the search bar
+        'target' => 'item-list-accordion', // ID of the accordion target
+        'accordion_id' => 'item-list-accordion', // ID of the accordion div
     ], $atts);
 
-    return '<input type="text" id="' . esc_attr($atts['id']) . '" class="search-bar" placeholder="' . esc_attr($atts['placeholder']) . '" oninput="filterItems(\'' . esc_attr($atts['target']) . '\')">';
+    // Render the search bar and the accordion container
+    return '
+        <div class="item-list-sidebar-container">
+            <input type="text" id="' . esc_attr($atts['id']) . '" class="search-bar" 
+                placeholder="' . esc_attr($atts['placeholder']) . '" 
+                oninput="filterItems(\'' . esc_attr($atts['target']) . '\')">
+            <div class="item-list-sidebar" id="' . esc_attr($atts['accordion_id']) . '">
+                <!-- JavaScript will populate this accordion dynamically -->
+            </div>
+        </div>';
 }
-add_shortcode('search_bar', 'render_search_bar');
+
+// Register the shortcode
+add_shortcode('search_bar_with_sidebar', 'render_search_bar_with_sidebar');
 
 // Selected Items Container Shortcode
 function render_selected_items_container($atts) {
