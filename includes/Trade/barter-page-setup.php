1b1790365d6aacfb88752fff63b5e52b97a65273
Thor-Jotunheim <138218017+Thor-Jotunheim@users.noreply.github.com>
2024-11-25 12:17:07 -0600

    trade

--- includes/Trade/barter-page-setup.php
+++ includes/Trade/barter-page-setup.php
@@ -14,7 +14,7 @@ function jotunheim_barter_page_shortcode() {
 function jotunheim_barter_page_half_screenshortcode() {
     // Start output buffering
     ob_start();
-    include plugin_dir_path(__FILE__) . 'barter-page-half-screen.html';
+    include plugin_dir_path(__FILE__) . 'jotunheim-barter-page-half-screen.html';
     return ob_get_clean();
 }
 
@@ -43,77 +43,3 @@ function jotunheim_enqueue_barter_scripts() {
     }
 }
 add_action('wp_enqueue_scripts', 'jotunheim_enqueue_barter_scripts');
-
-// Search Bar and Accordion Shortcode
-function render_search_bar_with_sidebar($atts) {
-    $atts = shortcode_atts([
-        'id' => 'item-search',
-        'placeholder' => 'Search items...',
-        'accordion_id' => 'item-list-accordion',
-    ], $atts);
-
-    ob_start();
-    ?>
-    <div class="item-list-sidebar-container">
-        <input type="text" id="<?php echo esc_attr($atts['id']); ?>" 
-            class="search-bar" 
-            placeholder="<?php echo esc_attr($atts['placeholder']); ?>" 
-            oninput="filterItems('<?php echo esc_attr($atts['accordion_id']); ?>')">
-        <div class="item-list-sidebar" id="<?php echo esc_attr($atts['accordion_id']); ?>">
-            <!-- JavaScript will populate this accordion dynamically -->
-        </div>
-    </div>
-    <?php
-    return ob_get_clean();
-}
-add_shortcode('search_bar_with_sidebar', 'render_search_bar_with_sidebar');
-
-// Selected Items Container Shortcode
-function render_selected_items_container($atts) {
-    $atts = shortcode_atts([
-        'id' => 'selected-items-container',
-        'background' => '/wp-content/uploads/Jotunheim-magic/icons/container-background.png'
-    ], $atts);
-
-    return '<div id="' . esc_attr($atts['id']) . '" class="selected-items-panel" style="background: url(' . esc_url($atts['background']) . ') no-repeat center center; background-size: contain;">
-                <!-- Items will be added dynamically via JavaScript -->
-            </div>';
-}
-add_shortcode('selected_items_container', 'render_selected_items_container');
-
-// Calculation Panel Shortcode
-function render_calculation_panel() {
-    return '<div class="calculation-panel">
-                <div class="trader-total trader1-total">
-                    <div>Total for Trader 1:</div>
-                    <div><span id="trader1-total-display">
-                        <div style="text-align: center;">
-                            Coins<br>
-                            <small>or</small><br>
-                            Ymir Flesh & Coins
-                        </div>
-                    </span></div>
-                </div>
-                <div class="trade-difference">
-                    <div>Trade Difference:</div>
-                    <div><span id="trade-difference">
-                        <div style="text-align: center;">
-                            Coins<br>
-                            <small>or</small><br>
-                            Ymir Flesh & Coins
-                        </div>
-                    </span></div>
-                </div>
-                <div class="trader-total trader2-total">
-                    <div>Total for Trader 2:</div>
-                    <div><span id="trader2-total-display">
-                        <div style="text-align: center;">
-                            Coins<br>
-                            <small>or</small><br>
-                            Ymir Flesh & Coins
-                        </div>
-                    </span></div>
-                </div>
-            </div>';
-}
-add_shortcode('calculation_panel', 'render_calculation_panel');
