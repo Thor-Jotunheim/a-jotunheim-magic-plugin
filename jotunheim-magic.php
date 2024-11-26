9f109e9fdb7ed9f134ba0120cb95ed3bba422808
Thor-Jotunheim <138218017+Thor-Jotunheim@users.noreply.github.com>
2024-11-26 07:48:38 -0600

    update

--- jotunheim-magic.php
+++ jotunheim-magic.php
@@ -60,8 +60,8 @@ include_once(plugin_dir_path(__FILE__) . 'includes/EventZone/eventzones-add-new-
 
 // Include Trade and Barter files
 include_once(plugin_dir_path(__FILE__) . 'includes/Trade/trade-api-get.php');
-include_once(plugin_dir_path(__FILE__) . 'includes/trade/barter-interface.php');
-include_once(plugin_dir_path(__FILE__) . 'includes/trade/trade-interface.php');
+include_once(plugin_dir_path(__FILE__) . 'includes/Trade/barter-interface.php');
+include_once(plugin_dir_path(__FILE__) . 'includes/Trade/trade-interface.php');
 
 
 // Include ledger files
