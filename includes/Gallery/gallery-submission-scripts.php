75848a32b3bd53340be149e11b3529936949e481
Thor-Jotunheim <138218017+Thor-Jotunheim@users.noreply.github.com>
2024-12-18 07:44:13 -0600

    update

--- includes/Gallery/gallery-submission-scripts.php
+++ includes/Gallery/gallery-submission-scripts.php
@@ -5,44 +5,58 @@ function handle_gallery_submission() {
     }
 
     // Validate input
-    if (empty($_POST['build_name']) || empty($_POST['description']) || empty($_POST['created_by']) || empty($_FILES['photos']['name']) || count($_FILES['photos']['name']) < 4) {
+    if (empty($_POST['build_name']) || empty($_POST['description']) || empty($_FILES['photos']['name'])) {
         wp_die('All fields are required, and you must upload at least 4 photos.');
+    }
+
+    if (count(array_filter($_FILES['photos']['name'])) < 4) {
+        wp_die('You must upload at least 4 photos.');
     }
 
     // Process uploaded photos
     $photo_ids = [];
     foreach ($_FILES['photos']['name'] as $index => $photo_name) {
-        $file = [
-            'name'     => $_FILES['photos']['name'][$index],
-            'type'     => $_FILES['photos']['type'][$index],
-            'tmp_name' => $_FILES['photos']['tmp_name'][$index],
-            'error'    => $_FILES['photos']['error'][$index],
-            'size'     => $_FILES['photos']['size'][$index],
-        ];
-
-        $upload = wp_handle_upload($file, ['test_form' => false]);
-        if (isset($upload['file'])) {
-            $attachment = [
-                'post_mime_type' => $upload['type'],
-                'post_title'     => sanitize_file_name($photo_name),
-                'post_content'   => '',
-                'post_status'    => 'inherit',
+        if ($_FILES['photos']['error'][$index] === UPLOAD_ERR_OK) {
+            $file = [
+                'name'     => $_FILES['photos']['name'][$index],
+                'type'     => $_FILES['photos']['type'][$index],
+                'tmp_name' => $_FILES['photos']['tmp_name'][$index],
+                'error'    => $_FILES['photos']['error'][$index],
+                'size'     => $_FILES['photos']['size'][$index],
             ];
-            $photo_ids[] = wp_insert_attachment($attachment, $upload['file']);
+
+            $upload = wp_handle_upload($file, ['test_form' => false]);
+            if (isset($upload['file'])) {
+                $attachment = [
+                    'post_mime_type' => $upload['type'],
+                    'post_title'     => sanitize_file_name($photo_name),
+                    'post_content'   => '',
+                    'post_status'    => 'inherit',
+                ];
+                $attachment_id = wp_insert_attachment($attachment, $upload['file']);
+                require_once ABSPATH . 'wp-admin/includes/image.php';
+                wp_generate_attachment_metadata($attachment_id, $upload['file']);
+                wp_update_attachment_metadata($attachment_id, wp_generate_attachment_metadata($attachment_id, $upload['file']));
+                $photo_ids[] = $attachment_id;
+            }
         }
     }
 
+    // Generate gallery shortcode for the post content
+    $gallery_shortcode = '';
+    if (!empty($photo_ids)) {
+        $gallery_shortcode = '[gallery ids="' . implode(',', $photo_ids) . '"]';
+    }
+
     // Create draft post
+    $post_content = sanitize_textarea_field($_POST['description']) . "\n\n" . $gallery_shortcode;
     $post_id = wp_insert_post([
         'post_title'   => sanitize_text_field($_POST['build_name']),
-        'post_content' => sanitize_textarea_field($_POST['description']),
+        'post_content' => $post_content,
         'post_status'  => 'draft',
         'post_type'    => 'post', // Or a custom post type like 'gallery'
-        'meta_input'   => [
-            'created_by' => sanitize_text_field($_POST['created_by']),
-            'photo_ids'  => $photo_ids,
-        ],
-        'page_template' => 'page-photo-gallery-submissions.php', // Assign your custom template
+        'post_category' => [get_cat_ID('Player Builds')], // Set default category
+        'post_author'  => get_current_user_id(), // Set the author to the current logged-in user
     ]);
 
     if ($post_id) {
