21834d59347d38ce7b37cda1d254a869b070cf1d
Thor-Jotunheim <thorfromjotunheim@gmail.com>
2025-05-05 19:05:46 -0500

    save

--- includes/KB/basepress-header-override.php
+++ includes/KB/basepress-header-override.php
@@ -19,8 +19,9 @@ function jotunheim_basepress_use_wp_theme_parts() {
     add_action('basepress_header', 'get_header', 1);
     add_action('basepress_footer', 'get_footer', 1);
     
-    // Add CSS to hide any BasePress headers that might still appear
+    // Add CSS and JS fixes
     add_action('wp_head', 'jotunheim_basepress_header_css');
+    add_action('wp_footer', 'jotunheim_basepress_js_fixes', 99);
 }
 
 /**
@@ -36,7 +37,59 @@ function jotunheim_basepress_header_css() {
         .bpress-header {
             display: none !important;
         }
+        
+        /* Ensure theme header is visible */
+        .site-header, 
+        header.wp-block-template-part {
+            display: block !important;
+            visibility: visible !important;
+        }
+        
+        /* Fix main content area spacing */
+        .bpress-wrap {
+            margin-top: 20px;
+        }
+        
+        /* Make sure sidebar doesn't conflict with theme elements */
+        .bpress-sidebar.fixedsticky-on {
+            z-index: 50;
+        }
     </style>
+    <?php
+}
+
+/**
+ * Add JavaScript fixes to counter BasePress JS that might interfere with theme header
+ */
+function jotunheim_basepress_js_fixes() {
+    if (!function_exists('is_basepress') || !is_basepress()) {
+        return;
+    }
+    ?>
+    <script>
+    jQuery(document).ready(function($) {
+        // Ensure FixedSticky doesn't apply to the header
+        if (typeof FixedSticky !== 'undefined') {
+            // Override the fixedsticky initialization for non-sidebar elements
+            var originalFixedSticky = $.fn.fixedsticky;
+            $.fn.fixedsticky = function() {
+                if ($(this).hasClass('bpress-sidebar')) {
+                    return originalFixedSticky.apply(this, arguments);
+                }
+                return this;
+            };
+        }
+        
+        // Make sure the theme header remains visible
+        $('.site-header, header.wp-block-template-part').css({
+            'display': 'block',
+            'visibility': 'visible'
+        });
+        
+        // Hide BasePress header if it still exists
+        $('.bpress-header').hide();
+    });
+    </script>
     <?php
 }
 
