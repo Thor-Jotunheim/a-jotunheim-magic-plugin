ced28fd3510c22bf8360fb7b2a8ada5d0bb3e14a
Thor-Jotunheim <thorfromjotunheim@gmail.com>
2025-08-22 12:36:41 -0500

    save

--- jotunheim-magic.php
+++ jotunheim-magic.php
@@ -181,22 +181,6 @@ function jotunheim_magic_assign_capabilities() {
 }
 
 register_activation_hook(__FILE__, 'jotunheim_magic_assign_capabilities');
-
-// AJAX handler for getting weather configuration
-function get_weather_config() {
-    wp_send_json_success(array(
-        'api_enabled' => get_option('weather_api_enabled', false),
-        'api_endpoint' => get_option('weather_api_endpoint', ''),
-        'manual_enabled' => get_option('weather_manual_enabled', false),
-        'manual_start_day' => get_option('weather_manual_start_day', 1),
-        'manual_start_date' => get_option('weather_manual_start_date', '2025-08-22T00:00'),
-        'manual_progression' => get_option('weather_manual_progression', 'static'),
-        'server_start_date' => get_option('weather_server_start_date', '2025-08-01T19:30')
-    ));
-}
-
-add_action('wp_ajax_get_weather_config', 'get_weather_config');
-add_action('wp_ajax_nopriv_get_weather_config', 'get_weather_config');
 add_action('admin_init', 'jotunheim_magic_assign_capabilities');
 
 // Temporarily elevate editor capabilities on specific editor pages
