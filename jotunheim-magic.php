fac40242e5c2ce67390579e55cae8768053e45a6
Thor-Jotunheim <thorfromjotunheim@gmail.com>
2025-09-24 20:09:30 -0500

    fix: Match button size and force green with inline styles v0.9.4.8.42

--- jotunheim-magic.php
+++ jotunheim-magic.php
@@ -4,7 +4,7 @@
 /*
 Plugin Name: A Jotunheim Magic Plugin
 Description: A plugin to manage the item list and editor for Jotunheim.
- * Version: 0.9.4.8.41
+ * Version: 0.9.4.8.42
 Author: Thor
 */
 
