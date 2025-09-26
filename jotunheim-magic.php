49b7f9e879de1c1f87b81a0e6d9dd2e701e4fcfb
Thor-Jotunheim <thorfromjotunheim@gmail.com>
2025-09-26 12:09:00 -0500

    v0.9.5.1.11 - Hide unnecessary Unified Teller page title

--- jotunheim-magic.php
+++ jotunheim-magic.php
@@ -4,7 +4,7 @@
 /*
 Plugin Name: A Jotunheim Magic Plugin
 Description: A plugin to manage the item list and editor for Jotunheim.
- * Version: 0.9.5.1.10
+ * Version: 0.9.5.1.11
 Author: Thor
 */
 
