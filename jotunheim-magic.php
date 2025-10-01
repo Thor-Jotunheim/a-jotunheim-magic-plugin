bd6a5f2af1ccb099857650b9df804367c34242fd
Thor-Jotunheim <thorfromjotunheim@gmail.com>
2025-10-01 09:12:04 -0500

    v0.9.6.0.21 - VERSION: Update version in main plugin file

--- jotunheim-magic.php
+++ jotunheim-magic.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: A Jotunheim Magic Plugin
 Description: A plugin to manage the item list and editor for Jotunheim.
- * Version: v0.9.6.0.20
+ * Version: v0.9.6.0.21
  * Description: A comprehensive plugin that manages a Valheim gaming server with Discord authentication, game mechanics, and player economy systems. [WORKING-CARDS-AS-TABLE-ROWS]
 Author: Thor
 */
